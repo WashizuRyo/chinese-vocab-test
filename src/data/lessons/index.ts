07cce85b6c41d2a839df9584613e93c17b7f2e51
Ryo Washizu <wasizuryo@gmail.com>
2026-06-29 15:44:45 +0900

    feat: add lesson 6 vocabulary audio

--- src/data/lessons/index.ts
+++ src/data/lessons/index.ts
@@ -4,9 +4,18 @@ import { lesson02 } from "./lesson02";
 import { lesson03 } from "./lesson03";
 import { lesson04 } from "./lesson04";
 import { lesson05 } from "./lesson05";
+import { lesson06 } from "./lesson06";
 import { number } from "./number";
 
-export const lessons: Lesson[] = [number, lesson01, lesson02, lesson03, lesson04, lesson05];
+export const lessons: Lesson[] = [
+  number,
+  lesson01,
+  lesson02,
+  lesson03,
+  lesson04,
+  lesson05,
+  lesson06,
+];
 
 export function getLesson(id: string): Lesson | undefined {
   return lessons.find((lesson) => lesson.id === id);
